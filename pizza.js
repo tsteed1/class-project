2b97534f8b076bf7cfb3e363c369b4e687a941b3
Travis Steed <steed0293@gmail.com>
2020-09-15 23:06:54 -0700

    made changes

--- pizza.js
+++ pizza.js
@@ -1,3 +1,4 @@
+'use strict'
 
 // var response = prompt('Do you like to eat pizza?');
 // var message;
@@ -18,8 +19,13 @@ function getPizza() {
     var response = prompt('Do you like to eat pizza?');
     var message;
 
+    while(response != 'yes' && response != 'no') {
+        message = prompt('Do you like to eat pizza? BE SPECIFIC')
+    }
+
     if (response === 'yes') {
-    message = 'Great! Let us show you some of the favorites in the Seattle area!';
+    item = "<img src=../images/creepypizza.jpg">;
+        item = 'Great! Let us show you some of the favorites in the Seattle area!';
     }
     else if (response === 'no') {
         message = 'I am sorry to hear that!';
@@ -31,4 +37,25 @@ function getPizza() {
     return document.write('<h3>' + message + '</h3>');
 }
 
+function getCount() {
+    var count = ('How many slices of pizza would you like?');
+    while(isNaN(count)) {
+        count = prompt("PLEASE ENTER A NUMBER. How many would you like?");
+    }
+    return count;
+}
+getCount();
 getPizza();
+
+function showOrder() {
+    var result = '';
+    var itemType = getPizza();
+
+    for(var i =0; i < total; i++) {
+        result = result + '<p>' + itemType + '</p>'
+    }
+
+    return document.write(result);
+}
+
+showOrder();
